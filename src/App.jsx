b6cb8070077d4fca3e5011ad96623174598e8b47
andre <driekie248@gmail.com>
2026-05-29 14:48:19 +0200

    feat: footer changed to svg and added active color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { useRef } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
+import Search from "./pages/Search/Search";
 
 function Player() {
   const audioRef = useRef(null);
@@ -46,15 +48,48 @@ function Player() {
   );
 }
 
+function PageWrapper({ children }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: 30 }}
+      animate={{ opacity: 1, x: 0 }}
+      exit={{ opacity: 0, x: -30 }}
+      transition={{ duration: 0.2 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <Header />
 
       <div className="page">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path="/"
+              element={
+                <PageWrapper>
+                  <Home />
+                </PageWrapper>
+              }
+            />
+
+            <Route
+              path="/search"
+              element={
+                <PageWrapper>
+                  <Search />
+                </PageWrapper>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
       </div>
 
       <Footer />
