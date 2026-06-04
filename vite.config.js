014f60777492dfbe5e47e34717d9ebac87c0776b
andre <driekie248@gmail.com>
2026-06-04 11:54:27 +0200

    feat: removed pwa capebeleties

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,8 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      manifest: {
-        name: 'Music app',
-        short_name: 'Music app',
-        theme_color: '#242323',
-        background_color: '#242323',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    react()
   ]
 })
